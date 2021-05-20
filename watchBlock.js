8b28bdc8b9fcd3fd0aeaf5bf9e5dfe59df4ed502
Maggie Delano <maggie.delano@gmail.com>
2021-05-20 14:16:04 -0400

    Update watchBlock.js to create a function addPullWatchAtBlock that takes a blockID and page and then adds that block ref of the reference to the page

--- watchBlock.js
+++ watchBlock.js
@@ -1,13 +1,24 @@
-// add pull watch for block and put the block ref ID into a specified page based on page id
+// add pull watch for block and put the block ref ID into a specified page based on page name
 
-// right now the block uid and page uid need to be hardcoded, but easy to take it from here...
-// video: https://www.loom.com/share/b1936a272e3b4031b3d5a167631cc1a0
-roamAlphaAPI
+// old video: https://www.loom.com/share/b1936a272e3b4031b3d5a167631cc1a0
+
+
+function addPullWatchAtBlock(blockID,pageName){
+
+  var blockString = '[:block/uid "' + blockID + '"]'; 
+
+  console.log(blockString);
+
+  pageID = getPageIDFromPageName(pageName);
+
+  console.log(pageID);
+
+  var result = roamAlphaAPI
   .data
   .addPullWatch("[{:block/_refs [:block/string :block/uid]}]", 
-                '[:block/uid "BLOCK"]', function a(before, after)
+                blockString, function a(before, after)
                  {if(before === null){ // check for null / first reference
-				           newBlockRef = after[":block/_refs"][0][":block/uid"];
+                   newBlockRef = after[":block/_refs"][0][":block/uid"];
                  }
                   else { 
                    beforeUIDs = before[":block/_refs"].map(x => x[":block/uid"]);
@@ -16,11 +27,31 @@ roamAlphaAPI
                   }
   
                   window
-  						.roamAlphaAPI
-  						.createBlock(
-						{"location": 
-						{"parent-uid": "PAGE", 
-		 				"order": 0}, 
-	 					"block": 
-						{"string": '((' + newBlockRef + '))'}})
-})
+              .roamAlphaAPI
+              .createBlock(
+            {"location": 
+            {"parent-uid": pageID, 
+            "order": 0}, 
+            "block": 
+            {"string": '((' + newBlockRef + '))'}})
+});
+
+  console.log(result);
+}
+
+
+
+function getPageIDFromPageName(pageName){
+  var pageID = window.roamAlphaAPI.q(`
+  [:find ?pageID
+  :in $ ?pageName
+  :where
+    [?p :node/title ?pageName]
+    [?p :block/uid ?pageID]
+]`, pageName);
+
+  return pageID[0][0];
+}
+
+
+
